a2592618ad4ccbfba9c492b580cffbe6bced2ab8
vinicius225 <vinicius.guilherme225@gmail.com>
2022-09-21 14:21:57 -0300

    Ajustes no DbContext

--- Infra/ServicesDependeces.cs
+++ Infra/ServicesDependeces.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using Data;
 using Microsoft.EntityFrameworkCore;
+using Data.Repositories.Interfaces;
+using Data.Repositories;
 
 namespace Infra
 {
@@ -15,11 +17,11 @@ namespace Infra
         {
             var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
 
-            services.AddDbContext<AppDbContext>();
+            services.AddDbContext<AppDbContext>(opt => opt.UseMySql("server=localhost;user=root;password=1234;database=ef", serverVersion));
         }
         public static void RepositoriesDependeces(this IServiceCollection services)
         {
-
+            services.AddScoped<IMedicoRepository, MedicoRepository>();
         }
     }
 }
